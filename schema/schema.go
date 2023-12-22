9ecc3451bf445b395fefa52d823e406e47575540
Vova Kulikov <vova@sourcegraph.com>
2023-12-21 21:52:02 -0300

    Put new search filters UI behind feature flag

--- schema/schema.go
+++ schema/schema.go
@@ -2481,6 +2481,8 @@ type SettingsExperimentalFeatures struct {
 	GoCodeCheckerTemplates *bool `json:"goCodeCheckerTemplates,omitempty"`
 	// NewSearchNavigationUI description: Enables new experimental search UI navigation
 	NewSearchNavigationUI *bool `json:"newSearchNavigationUI,omitempty"`
+	// NewSearchResultFilters description: Enables new experimental search results filters
+	NewSearchResultFilters *bool `json:"newSearchResultFilters,omitempty"`
 	// NewSearchResultsUI description: Enables new experimental search results UI, such as preview panel feature and updated search and filter layouts.
 	NewSearchResultsUI *bool `json:"newSearchResultsUI,omitempty"`
 	// ProactiveSearchResultsAggregations description: Search results aggregations are triggered automatically with a search.
@@ -2548,6 +2550,7 @@ func (v *SettingsExperimentalFeatures) UnmarshalJSON(data []byte) error {
 	delete(m, "fuzzyFinderSymbols")
 	delete(m, "goCodeCheckerTemplates")
 	delete(m, "newSearchNavigationUI")
+	delete(m, "newSearchResultFilters")
 	delete(m, "newSearchResultsUI")
 	delete(m, "proactiveSearchResultsAggregations")
 	delete(m, "searchContextsQuery")
